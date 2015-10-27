63e0eae90baeb39421dfa7a70f8a947e6729fb78
Andrés Pereira <apereira@alabs.es>
2015-10-27 16:04:17 -0500

    [backups] one script

---
+++ 3.Backups/backup.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+# 
+# Script para la copia de seguridad para las tres plataformas de Gobierno Abierto
+# de la Prefectura de Carchi: Gobierno Abierto (rails), Datos Abiertos (ckan) y Atención Ciudadana (rails). 
+#
+# Autor: apereira@alabs.org
+# 
+# Para llamar al script y que diferencie de que directorio debe realizar la copia, 
+# hace falta hacerlo con dos parámetros obligatorios: 
+# 
+# backup.sh -t tipo -u url
+#
+# Las opciones de tipo son rails o ckan y de entorno production o staging.
+#
+# Para cada una de las platafomas con su tipo sería: 
+#
+# Atención Ciudadana: 
+# $ backup.sh -t rails -u atencionciudadana.carchi.gob.ec
+# $ backup.sh -t rails -u beta.atencionciudadana.carchi.gob.ec
+# 
+# Gobierno Abierto: 
+# $ backup.sh -t rails -u gobiernoabierto.carchi.gob.ec 
+# $ backup.sh -t rails -u beta.gobiernoabierto.carchi.gob.ec
+# 
+# Datos Abiertos: 
+# $ backup.sh -t ckan -u datosabiertos.carchi.gob.ec
+# $ backup.sh -t ckan -u beta.datosabiertos.carchi.gob.ec
+# 
+# Durante la instalación inicial pedirá la configuración de la cuenta de Dropbox, 
+# debemos seguir los pasos y darle permisos a la Aplicación que creemos. 
+#
+################################################################################
+
+usage() { 
+  # muestra ayuda cuando faltan parámetros
+  echo "Usage: $0 [-t <ckan|rails>] [-u <url>]" 1>&2; exit 1; 
+}
+
+install_dropbox_uploader() {
+  # comprueba si existe el script de Dropbox, si no lo tiene lo descarga y configura.
+  if [ ! -f /usr/local/bin/dropbox_uploader.sh ] ; then 
+    which curl || sudo apt-get install -y curl 
+    curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o /usr/local/bin/dropbox_uploader.sh
+    chmod +x /usr/local/bin/dropbox_uploader.sh 
+    dropbox_uploader.sh
+  fi
+}
+
+create_dir() {
+  # comprueba si existe el directorio, si no existe lo crea 
+  # ve al directorio
+  dir=$1
+  [ ! -d $dir ] && mkdir -p $dir
+  cd $dir
+}
+
+backup_postgres() {
+  # realiza
+  cd /usr/local/backup/dump/
+  chown -R postgres: /usr/local/backup/dump/
+  sudo -u postgres pg_dumpall > dump.sql
+  rm dump.sql.gz
+  gzip dump.sql 
+}
+
+backup_file() {
+  # comprime un fichero o directorio
+  # acepta un parametro, fichero a comprimir
+  file=$1
+  name=$(basename ${file})
+  tar czfP ${name}.tgz ${file}
+}
+
+upload_to_dropbox() { 
+  # sube un fichero a dropbox
+  # acepta un parametro, fichero a subir
+  dropbox_uploader.sh -q upload $1 backup.${URL}.${1}
+}
+
+init_backup () {
+  # pre-requisites
+  install_dropbox_uploader 
+  create_dir /usr/local/backup/dump/
+
+  # backing up database...
+  backup_postgres
+
+  case "$BACKUP_TYPE" in 
+    # si la aplicación es ckan
+    ckan) 
+      # backing up files...
+      backup_file /etc
+      backup_file /var/lib/ckan
+
+      # uploading to dropbox...
+      upload_to_dropbox dump.sql.gz
+      upload_to_dropbox etc.tgz 
+      upload_to_dropbox ckan.tgz 
+      ;; 
+    # si la aplicación es ruby on rails
+    rails)
+      # backing up files...
+      backup_file /etc
+      backup_file /var/www/${URL}/shared/config/
+      backup_file /var/www/${URL}/shared/public/
+
+      # uploading to dropbox...
+      upload_to_dropbox dump.sql.gz
+      upload_to_dropbox etc.tgz 
+      upload_to_dropbox config.tgz 
+      upload_to_dropbox public.tgz
+      ;;
+  esac
+
+}
+
+# toma opciones de la linea de comando
+while getopts ":t:u:" o; do
+    case "${o}" in
+        t)
+            BACKUP_TYPE=${OPTARG}
+            ((BACKUP_TYPE == rails || BACKUP_TYPE == ckan)) || usage
+            ;;
+        u)
+            URL=${OPTARG} || usage
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${BACKUP_TYPE}" ] || [ -z "${URL}" ]; then
+    usage
+fi
+
+if [ "$(id -u)" != "0" ]; then
+   echo "Este script debe ejecutarse como root" 1>&2
+   exit 1
+fi
+
+init_backup
+
--- 3.Backups/restore/datosabiertos.bash
+++ 3.Backups/restore/datosabiertos.bash
@@ -1,12 +1,104 @@
+#!/bin/bash
+# 
+# Script para la restauración de la copia de seguridad de la plataforma de Atención Ciudadana de Carchi
+# Autor: apereira@alabs.org
+# Fecha: 8 de Septiembre de 2015 
+#
+# Restaura la última copia de seguridad que encuentre en /usr/local/backup/dump/dump.sql.gz
+# 
+# Hay que tener en cuenta una cosa de la configuración de ALTER_PERMISSIONS. 
+#
+# Si es ALTER_PERMISSIONS=true significa que aparte de restaurar la base de datos y ficheros del backup 
+# también se encargará de renombrar los usuarios y base de datos para que funcione en el servidor. 
+# Esto es para el caso de un restore de la maquina de prod-datosabiertos a stag-datosabiertos.
+#
+# Por otro lado si se quiere restaurar la base de datos de producción en producción (restaurar una copia 
+# de seguridad de prod-datosabiertos a prod-datosabiertos, o sea, se conectaría al mismo usuario y contraseña
+# que ya tiene.), lo que se debe hacer es poner ALTER_PERMISSIONS=false.
+#  
+ 
 
+ALTER_PERMISSIONS=true
+# ALTER_PERMISSIONS=false
+DB_NAME_OLD=datosabier_stag
+DB_NAME_NEW=datosabier_prod
+WEBSERVER=apache2
 
-psql -f dump.2015092501.sql 
-psql
+################################################################################
 
-ALTER DATABASE "datosabier_prod" RENAME "datosabier_old";
-ALTER DATABASE "datosabier_stag" RENAME "datosabier_prod";
-ALTER DATABASE datosabier_prod OWNER TO datosabier_prod; 
-\c datosabier_prod;
-REASSIGN OWNED BY datosabier_stag TO datosabier_prod;
+install_dropbox_uploader() {
+  if [ ! -f /usr/local/bin/dropbox_uploader.sh ] ; then 
+    which curl || sudo apt-get install -y curl 
+    curl "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o /usr/local/bin/dropbox_uploader.sh
+    chmod +x /usr/local/bin/dropbox_uploader.sh 
+    dropbox_uploader.sh
+  fi
+}
 
+restore_from_dropbox() {
+  # acepta un parametro, fichero a subir
+  dropbox_uploader.sh download $1 $1
+}
 
+create_dir() {
+  # comprueba si existe el directorio, si no existe lo crea 
+  # ve al directorio
+  dir=$1
+  [ ! -d $dir ] && mkdir -p $dir
+  cd $dir
+}
+
+if [ -d /usr/local/backup/restore/ ] ; then
+  echo "Ya hay un restore realizado. Por favor comprueba el directorio /usr/local/backup/restore/, renombralo o borralo y vuelve a ejecutar este script."
+  exit 1;
+fi
+
+restore_postgres() {
+  gunzip dump.sql.gz
+  today=$(date +%Y%m%d)
+  dump=/var/lib/postgresql/dump.sql
+  
+  # paramos el webserver porque sino no nos va dejar cambiar la DB con conexiones abiertas
+  service ${WEBSERVER} stop
+  cp dump.sql /var/lib/postgresql
+  chown postgres: $dump
+  sudo -u postgres psql -f $dump 
+
+  if [ ${ALTER_PERMISSIONS} ] ; then
+    sudo -u postgres psql -c "ALTER DATABASE ${DB_NAME_OLD} RENAME TO datosabier_${today};"
+    sudo -u postgres psql -c "ALTER DATABASE ${DB_NAME_NEW} RENAME TO ${DB_NAME_OLD};"
+    sudo -u postgres psql -c "ALTER DATABASE ${DB_NAME_OLD} OWNER TO ${DB_NAME_OLD};"
+    sudo -u postgres psql ${DB_NAME_OLD} -c "REASSIGN OWNED BY ${DB_NAME_NEW} TO ${DB_NAME_OLD};"
+  fi
+
+  service ${WEBSERVER} start
+}
+
+restore_files() {
+  tar xzfP etc.tgz
+  tar xzfP ckan.tgz
+  today=$(date +%Y%m%d)
+  mv /var/lib/ckan/ /var/lib/ckan.${today}
+  mv /usr/local/backup/restore/var/lib/ckan /var/lib/
+}
+
+################################################################################
+
+init_restore () {
+  # pre-requisites
+  install_dropbox_uploader 
+  create_dir /usr/local/backup/restore/
+
+  # downloading from dropbox...
+  restore_from_dropbox dump.sql.gz
+  restore_from_dropbox etc.tgz
+  restore_from_dropbox ckan.tgz
+
+  # restoring database...
+  restore_postgres
+
+  # restoring files...
+  restore_files
+}
+
+init_restore
